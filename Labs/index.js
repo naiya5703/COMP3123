4db4650ed934d9d29a26eb9081821a4d57cd505f
naiya5703 <156104698+naiya5703@users.noreply.github.com>
2024-09-29 18:58:31 -0400

    Add files via upload

--- Labs/index.js
+++ Labs/index.js
@@ -1,49 +1,37 @@
-var http = require("http");
+const express = require('express');
+const app = express();
+const SERVER_PORT = 3000;
 
-// To import the Employee Module
-const Employee = require("./Employee"); 
-console.log("Lab 03 - NodeJs");
-
-// Define Server Port
-const port = process.env.PORT || 8081;
-
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-
-    if (req.method !== 'GET') {
-        res.statusCode = 405;
-        res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
-        return;
-    }
-
-    if (req.url === '/') {
-        res.setHeader('Content-Type', 'text/html'); 
-        res.end("<h1>Welcome to Lab Exercise 03</h1>");            //Display Message
-        return;
-    }
-
-    if (req.url === '/employee') {
-        const employees = Employee.getAllEmployees();
-        res.end(JSON.stringify(employees));                        // Display details of all employees
-        return;
-    }
-
-    if (req.url === '/employee/names') {
-        const employeeNames = Employee.getEmployeeNames();
-        res.end(JSON.stringify(employeeNames));                    //Display "firstName + lastName" of employee in ascending order 
-        return;
-    }
-
-    if (req.url === '/employee/totalsalary') {
-        const totalSalary = Employee.getTotalSalary();
-        res.end(JSON.stringify({ total_salary: totalSalary }));    //Display total salary of all employees
-        return;
-    }
-
-    res.statusCode = 404;
-    res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
+// http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
 });
 
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+// http://localhost:3000/hello
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Express JS</h1>');
 });
+
+// Query String Parameters
+// http://localhost:3000/user?firstname=Naiya&lastname=Patel
+app.get('/user', (req, res) => {
+    console.log(req.query);
+    let firstname = req.query.firstname;
+    let lastname = req.query.lastname;
+
+    res.json({ firstname, lastname });
+    res.send(`<h1>First Name: ${firstname}, Last Name: ${lastname}</h1>`);
+})
+
+// Path Parameters
+// http://localhost:3000/user/Naiya/Patel
+app.post('/user/:firstname/:lastname', (req, res) => {
+    let firstname = req.params.firstname;
+    let lastname = req.params.lastname;
+
+    res.send(`<h1>First Name: ${firstname}, Last Name: ${lastname}</h1>`);
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+})
