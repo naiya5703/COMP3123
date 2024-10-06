a3c99bd964d515c313e86ea063d13e943eaaf90f
naiya5703 <156104698+naiya5703@users.noreply.github.com>
2024-10-06 19:34:22 -0400

    Add files via upload

--- Labs/index.js
+++ Labs/index.js
@@ -1,37 +1,86 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
-const SERVER_PORT = 3000;
+const router = express.Router();
 
-// http://localhost:3000/
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+app.use(express.json());
+
+//  Serve home.html page 
+router.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-// http://localhost:3000/hello
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello Express JS</h1>');
+// Return all details from user.json file to client as JSON format
+router.get('/profile', (req, res) => {
+  fs.readFile(path.join(__dirname, 'user.json'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Server Error: Unable to read user data',
+      });
+    }
+    res.json(JSON.parse(data)); 
+  });
 });
 
-// Query String Parameters
-// http://localhost:3000/user?firstname=Naiya&lastname=Patel
-app.get('/user', (req, res) => {
-    console.log(req.query);
-    let firstname = req.query.firstname;
-    let lastname = req.query.lastname;
+// Read data from user.json file
+// For login 
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-    res.json({ firstname, lastname });
-    res.send(`<h1>First Name: ${firstname}, Last Name: ${lastname}</h1>`);
-})
+  fs.readFile(path.join(__dirname, 'user.json'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Server Error: Unable to read user data',
+      });
+    }
 
-// Path Parameters
-// http://localhost:3000/user/Naiya/Patel
-app.post('/user/:firstname/:lastname', (req, res) => {
-    let firstname = req.params.firstname;
-    let lastname = req.params.lastname;
+    const user = JSON.parse(data);
 
-    res.send(`<h1>First Name: ${firstname}, Last Name: ${lastname}</h1>`);
-})
+    if (username !== user.username) {
+      return res.status(401).json({
+        status: false,
+        message: 'User Name is invalid',
+      });
+    }
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
-})
+    if (password !== user.password) {
+      return res.status(401).json({
+        status: false,
+        message: 'Password is invalid',
+      });
+    }
+
+    res.json({
+      status: true,
+      message: 'User is valid',
+    });
+  });
+});
+
+//For logout
+router.get('/logout/:username', (req, res) => {
+  const username = req.params.username;
+  res.json({
+    message: `${username} successfully logged out.`,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); 
+  res.status(500).json({
+    status: false,
+    message: 'Server Error',
+  });
+});
+
+app.use('/', router);
+
+// Start the server at port 8086
+const PORT = process.env.PORT || 8086;
+app.listen(PORT, () => {
+  console.log('Web Server is listening at port ' + PORT);
+});
